61dee675c7d36674d6fe2aeb6705aeca4b6eb73a
toothsy <c.j.atharva@gmail.com>
2021-06-24 12:05:13 +0530

    added transition, aboutUS and svgs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { Menu, Dropdown }from 'semantic-ui-react'
 import sdiHomeLogo from '../svgs/sdiHomeLogo.svg'
 
 function Navbar(){
@@ -13,16 +12,6 @@ function Navbar(){
         }else{
             x.className = "sdi_navbar";
         }
-        return(
-        <Menu secondary >
-            <Dropdown text='Shopping' pointing className='link item'>
-            <Dropdown.Menu>
-                <Dropdown.Item>Home Goods</Dropdown.Item>
-                <Dropdown.Item>Bedroom</Dropdown.Item>
-            </Dropdown.Menu>
-            </Dropdown>
-        </Menu>
-        )
     };   
 
     return(
@@ -40,9 +29,7 @@ function Navbar(){
             <NavLink to="/blogs" id="sdi_nav_last" activeClassName="is-active" className="sdi_navbar1">Blogs</NavLink>
             <NavLink to="/about" activeClassName="is-active" className="sdi_navbar1">About</NavLink>
             <NavLink to="/" exact activeClassName="is-active" className="sdi_navbar1">Home</NavLink>
-            <svg className="home_svg1" viewBox="0 0 593 1512" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M285.111 407.353C-136.561 582.299 2.62679 950.518 117.808 1076.15C117.808 1076.15 803.426 1869.9 820.293 1315.07C837.16 760.243 769.692 -64 769.692 -64L153.821 -44.0062C321.426 -35.1756 696.543 236.656 285.111 407.353Z" fill="#B2FFF7"/>
-            </svg>
+            
         </header>
         </>
     )
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import gS from '../svgs/groupSelfie.svg'
+import wT from '../svgs/wavyTop.svg'
+import wB from '../svgs/wavyBottom.svg'
 function About(){
     return(
-        <div>About</div>
+        <>
+            <img id="wT" src={wT} alt="" />
+            <h1>About Us</h1>
+            <img src={gS} className="gS" alt="" />
+            <img  id="wB" src={wB} alt="" />
+        </>
     )
 }
 
-export default About;
+export default About;   
